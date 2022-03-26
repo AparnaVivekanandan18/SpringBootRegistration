3f03ac7b1d90280b9ee6e2a6b879da8d0cfd353c
Aparna-Vivekanandan <84588336+Aparna-Vivekanandan@users.noreply.github.com>
2022-03-25 23:44:26 -0300

    Initial Commmit Registration Module

---
+++ registration/src/main/java/com/registration/registration/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.registration.registration.repository;
+
+import com.registration.registration.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>
+{
+}
---
+++ registration/src/main/java/com/registration/registration/services/StudentService.java
@@ -0,0 +1,29 @@
+package com.registration.registration.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.registration.registration.repository.StudentRepository;
+import com.registration.registration.model.Student;
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repository;
+    public void save(Student student) {
+        repository.save(student);
+    }
+}
+//save and flush - search
+//@transaction ?
+// findByID - how to fetch data using another column
+// findByColumnName
+//What is hibernate Cache
+// cascading - hibernate main feature - how saving in database.
+//fetchTYpes - lazy fetch....
+// Handle the execeptions
+//EntityManager.getReference.
+// Decrypt the password and save it in the DB.
+// Authenticate the password.
+// Hiding the password and sending it in URL (JWT) in springboot.
+// Streams and optional.
+// immutable List
+// transient keyword ,
